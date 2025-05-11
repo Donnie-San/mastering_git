e9b541c00dc7879abdc6faf97d49b02f8d0dcc87
Donnie <donniesan76@gmail.com>
2025-05-11 15:57:39 +0700

    Added hunt and flee method

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 class Animal:
-    def __init__(self, health, speed):
+    def __init__(self, type, health, speed):
+        self.type = type
         self._health = health
         self._speed = speed
 
 class Prey(Animal):
-    def __init__(self, health, speed):
-        super().__init__(health, speed)
+    def __init__(self, type, health, speed):
+        super().__init__(type ,health, speed)
+    
+    def flee(self):
+        print(f"{self.type} is fleeing!")
 
 class Predator(Animal):
-    def __init__(self, health, speed):
-        super().__init__(health, speed)
+    def __init__(self, type, health, speed):
+        super().__init__(type, health, speed)
+
+    def hunt(self):
+        print(f"{self.type} is hunting!")
 
 class Fish(Prey, Predatorl):
-    def __init__(self, health, speed):
-        super().__init__(health, speed)
+    def __init__(self, health, speed, type = "Fish"):
+        super().__init__(type, health, speed)
