0b319bb798dd4717feea83e43a691f3d9eba659f
Donnie <donniesan76@gmail.com>
2025-05-11 15:07:58 +0700

    Add readme.md file

---
+++ main.py
@@ -0,0 +1,11 @@
+class Animal:
+    def __init__(self):
+        pass
+
+class Prey(Animal):
+    def __init__(self):
+        super().__init__()
+
+class Predator(Animal):
+    def __init__(self):
+        super().__init__()
